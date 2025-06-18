7182ca3ae8f1ad6c060e50bbecaa3857a95b1267
gwenael9 <gwenaelgueho@gmail.com>
2025-06-18 12:13:01 +0200

    ajout des dependances + test exo

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig } from "playwright-bdd";
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -8,11 +9,19 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const useBdd = process.env["PLAYWRIGHT_USE_BDD"] === "1";
+const specs = useBdd
+  ? defineBddConfig({
+      features: ["features/**/*.feature"],
+      steps: ["steps/**/*.ts", "support/**/*.ts"],
+    })
+  : "tests";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: specs,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,31 +31,31 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
--- e2e/tests/exercise.spec.ts
+++ e2e/tests/exercise.spec.ts
@@ -1,41 +1,46 @@
 import { test, expect } from "@playwright/test";
-import { login, logout } from "../helpers/auth";
-import { createExercise, deleteExercise, Exercise } from "../helpers/exercise";
+import { LoginPo } from "../pages/auth.po";
+import { userName } from "../tools/Auth";
 
-const exercise: Exercise = {
+const exercise = {
   title: "Exercice de test",
   content: "Contenu",
 };
 
-test("create a new exercise", async ({ page }) => {
-  await login(page);
+test.describe("Exercice", () => {
+  let loginPo: LoginPo;
 
-  await createExercise(page, exercise);
-  await deleteExercise(page);
+  test.beforeEach(async ({ page }) => {
+    loginPo = new LoginPo(page);
+    await loginPo.goTo();
+    await loginPo.logAsUser(userName.TOTO);
+  });
 
-  await logout(page);
-});
+  test("create a new exercise", async ({ page }) => {});
 
-test("create and add tour", async ({ page }) => {
-  await login(page);
-  await createExercise(page, exercise);
+  // test("create and add tour", async ({ page }) => {
+  //   await login(page);
+  //   await createExercise(page, exercise);
 
-  const addTour = page.getByRole("button").filter({ hasText: "À mon tour !" });
-  await expect(addTour).toBeVisible();
-  await addTour.click();
+  //   const addTour = page
+  //     .getByRole("button")
+  //     .filter({ hasText: "À mon tour !" });
+  //   await expect(addTour).toBeVisible();
+  //   await addTour.click();
 
-  await expect(
-    page.getByRole("heading", { name: "À vous de jouer !", level: 2 })
-  ).toBeVisible();
+  //   await expect(
+  //     page.getByRole("heading", { name: "À vous de jouer !", level: 2 })
+  //   ).toBeVisible();
 
-  const textArea = page.locator('p[data-placeholder="Type here..."]');
-  await expect(textArea).toBeVisible();
-  await textArea.fill("Mon texte de test");
+  //   const textArea = page.locator('p[data-placeholder="Type here..."]');
+  //   await expect(textArea).toBeVisible();
+  //   await textArea.fill("Mon texte de test");
 
-  const submitButton = page.getByRole("button", { name: "Soumettre" });
-  await expect(submitButton).toBeVisible();
-  await submitButton.click();
+  //   const submitButton = page.getByRole("button", { name: "Soumettre" });
+  //   await expect(submitButton).toBeVisible();
+  //   await submitButton.click();
 
-  // Vérifier que le contenu de l'exercice est visible
-  await expect(page.getByText("Mon texte de test")).toBeVisible();
+  //   // Vérifier que le contenu de l'exercice est visible
+  //   await expect(page.getByText("Mon texte de test")).toBeVisible();
+  // });
 });
